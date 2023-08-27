d16e7dad55bca499c8516cdcf0690f4244097406
Plolw <115927381+Plolw@users.noreply.github.com>
2023-08-27 20:00:29 +0200

    dfdf

--- Wiki/encyclopedia/views.py
+++ Wiki/encyclopedia/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
-
 from . import util
+from markdown2 import markdown
+from . import util
+from django import forms
+from django.http import HttpResponseRedirect
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
     })
 
+def entry(request, title):
+    if util.get_entry(title) == None:
+        return render(request, "encyclopedia/entry.html", {
+        "text": "<h1>Page not found</h1>"
+    })
+    text = markdown(util.get_entry(title))
+    return render(request, "encyclopedia/entry.html", {
+        "text": text
+    })
+
+def search(request):
+    if request.method == "POST":
+        entries = util.list_entries()
+        q = request.POST["q"]
+        print(q)
+        if util.get_entry(q) != None:
+            return HttpResponseRedirect("")
+        results = []
+        for entry in entries:
+            if q in entry:
+                results.append(entry)
+        return render(request, "encyclopedia/search.html", {
+        "entries": results
+    })
+    return render(request, "encyclopedia/search.html", {
+        "entries": util.list_entries()
+    })
