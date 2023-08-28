05f1e176388f69037456fdf16db1133b889b949e
Plolw <polpolortiz@gmail.com>
2023-08-28 18:09:20 +0200

    ytyt

--- Wiki/encyclopedia/urls.py
+++ Wiki/encyclopedia/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
     path("search", views.search, name="search"),
-    path("NewPage", views.new_page, name="new_page")
+    path("NewPage", views.new_page, name="new_page"),
+    path("Edit<str:title>", views.edit, name="entry")
 ]
