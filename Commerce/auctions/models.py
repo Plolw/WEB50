9d671cea1ca47add5e66b09c58228d49201df626
Plolw <115927381+Plolw@users.noreply.github.com>
2023-09-11 03:29:19 +0200

    uy

--- Commerce/auctions/models.py
+++ Commerce/auctions/models.py
@@ -18,3 +18,14 @@ class Listing(models.Model):
     startingBid = models.IntegerField()
     imageURL = models.URLField()
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default=OTHER)
+    watchers = models.ManyToManyField(User, blank=True, related_name="watchlisted")
+
+class Bid(models.Model):
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    listing_bidded = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    bid = models.IntegerField()
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    comment = models.CharField(max_length=300)
