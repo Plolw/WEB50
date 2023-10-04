4113d3e82838b8965abcdb0ad8a8e5360914e92b
Plolw <115927381+Plolw@users.noreply.github.com>
2023-10-05 00:14:53 +0200

    dsd

--- Network/network/views.py
+++ Network/network/views.py
@@ -63,6 +63,7 @@ def register(request):
     else:
         return render(request, "network/register.html")
     
+    
 def create(request):
     if request.method != "POST":
         return JsonResponse({"error": "Invalid request method"}, status=400)
@@ -72,24 +73,37 @@ def create(request):
     post.save()
     return JsonResponse({"message": "Post poested succesfully!"}, status=201)
 
+
 def posts(request, category):
     if request.method != "GET":
         return JsonResponse({"error": "Invalid request method"}, status=400)
     if category == "allposts":
         posts = Post.objects.all()
     elif category == "following":
-        user = request.user
+        user = request.user 
         following = user.following
         posts = Post.objects.filter(author__in=following).all()
     else:
         return JsonResponse({"error": "Requested page does not exist"}, status=400)
     return JsonResponse([pst.serialize() for pst in posts], safe=False)
 
+
 def post(request, post):
-    if request.method != "GET":
-        return JsonResponse({"error": "Invalid request method"}, status=400)
     try:
         post = Post.objects.get(user=request.user, id=post)
     except post.DoesNotExist:
         return JsonResponse({"error": "Post not found."}, status=404)
-    return JsonResponse(post.serialize(), safe=False) 
+    if request.method == "GET":
+        return JsonResponse(post.serialize(), safe=False)
+    elif request.method == "PUT":
+        data = json.loads(request.body)
+        if data.get("content") is not None:
+            p = Post.objects.get(id=post)
+            p.content = data["content"]
+        if data.get("likes") is not None:
+            p = Post.objects.get(id=post)
+            p.content = data["likes"]
+        p.save()
+        return HttpResponse(status=201)
+    else:
+        return JsonResponse({"error": "Must use GET or PUT methods"}, status=404)
